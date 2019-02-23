f83e7bc07fe3e94846d15a96a5270609f49f50f9
JiangNanMax <1321064814@qq.com>
2019-02-23 21:08:06 +0800

    数据展示表格

--- Query_Pane.py
+++ Query_Pane.py
@@ -35,6 +35,18 @@ class QueryPane(QWidget, Ui_Form):
         self.start_date_edit.setDate(QDate.currentDate())
         self.start_date_edit.setMinimumDate(QDate.currentDate())
 
+        # 设置表格的头部数据
+        model = QStandardItemModel(self.tickets_tv)
+        # 设置模型的头部数据
+        headers = ["车次", "出发站->到达站", "出发时间->到达时间", "历时", "商务座-特等座",
+                   "一等座","二等座", "高级软卧", "软卧-一等卧", "动卧","硬卧",
+                   "二等卧", "软座", "硬座", "无座", "其他"]
+        model.setColumnCount(len(headers))
+        for idx, header in enumerate(headers):
+            model.setHeaderData(idx, Qt.Horizontal, header)
+
+        self.tickets_tv.setModel(model)
+
     def query_tickets(self):
         print("查询票")
 
@@ -50,9 +62,17 @@ class QueryPane(QWidget, Ui_Form):
         print(purpose_codes)
 
         result, result_len = APITool.query_tickts(start_date, from_station_code, to_station_code, purpose_codes)
-        print(result_len)
-        for i in result:
-            print(i)
+        #print(result_len)
+        #for i in result:
+        #    print(i)
+
+        model = self.tickets_tv.model()
+        model.setRowCount(len(result))
+
+        for train_dic in result:
+            print(train_dic)
+
+        self.tickets_tv.setModel(model)
 
 if __name__ == '__main__':
     import sys
